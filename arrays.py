1fd3085d55c1711c8058f7f5b8f31e46adb0a4a6
BrankoSabo <ssabobranko@gmail.com>
2020-04-12 19:00:38 +0200

    Arrays.py #finished

--- arrays.py
+++ arrays.py
@@ -2,17 +2,19 @@
 # Arrays in Python are called Lists and during the script I will called them arrays
 # Writing a array
 A = ['yellow', 'red', 'blue', 'green', 'black']
+B = ['yellow', 'red', 'blue', 'green', 'black']
+C = ['']
 # We will be using the A array throughout the script
 # The is how the output looks like of a simple array
 print(A)
 # >>>Output: ['yellow', 'red', 'blue', 'green', 'black']
 
 # Displaying values from the string using their index
-print('The value on the index of 2 is: '+ A[2])
+print('\nThe value on the index of 2 is: '+ A[2])
 # >>>Output: The value on the index of 2 is blue
 
 # Slicing the array
-print('A[1:4] returns= '+ str(A[1:4]))
+print('\nA[1:4] returns= '+ str(A[1:4]))
 # = returns ['red', 'blue', 'green']
 print('A[2:] returns= '+ str(A[2:]))
 #= returns ['blue', 'green', 'black']
@@ -24,9 +26,51 @@ print('A[1:-1] returns= '+ str(A[1:-1]))
 #= returns ['red', 'blue', 'green']
 
 # Using the len() function will display the number of items inside of a array
-print('There are '+ str(len(A)) +' items in the A array')
+print('\nThere are ' + str(len(A)) +' items in the A array')
 
 # Sorting; sorted() is the only sort function in Python but it allows you to a lot of things,
 # for higher detail visit link:https://docs.python.org/3/howto/sorting.html
 # Simple Sort is just sorting the array values from lower to upper
-print('This ('+str(A)+') will be sorted like this: '+ str(sorted(A)))
+print('\nThis ('+str(A)+') will be sorted like this: ' + str(sorted(A)))
+
+# Reversing the array
+B.reverse()
+print('\n' + str(A) + ' reverses into ' + str(B))
+B.reverse()
+
+# Adding/Appending onto the end of the array
+B.append('tanana')
+print('\nAfter appending the new array looks like this: ' + str(B))
+
+# Inserting values into a list
+B.insert(5,'Tjasi')
+print('\nOn the 5th index we\'ve added the wanted value, so the array looks like this: ' + str(B))
+
+# Adding an array to a array
+B.extend(A)
+print('\nWe have added the A array onto the B array at its end: ' + str(B))
+
+# Removing the first found value from a array
+print('\n'+ str(B))
+B.remove('red')
+print('The first "red" value was removed from the array:')
+print(B)
+
+# Deleting a value by index
+del B[7]
+print('/nWe will remove the value of the 7th index:\n' + str(B))
+
+# Removing the last value for an array
+B.pop()
+print('\nUsing pop it will remove the last value from the array:\n' + str(B))
+
+# Counting the occurrences of a value in an array
+print('\nWe can check how many times there is "yellow" in the array: ' + str(B.count('yellow')))
+
+# Some use of IF statements
+if 'tanana' in B:
+    print('\nTjasi Tjasi Tanana\n')
+
+# Some use of FOR loops
+for item in B:
+    print(item)
